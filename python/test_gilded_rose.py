bfdcae8a56420d2628422b702093de01fcb5f988
cwfietz <cwfietz@calcna.ab.ca>
2022-03-29 18:45:37 -0600

    increment quality by one

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -8,7 +8,22 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        # self.assertEquals("foo", items[0].name)
+        self.assertEqual("foo", items[0].name)
+
+    def test_quality_degrades_by_two(self):
+        items = [Item("foo", 0, 2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_quality_degrades_by_one(self):
+        items = [Item("foo", 1, 2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(1, items[0].quality)
+
+
 
 if __name__ == '__main__':
     unittest.main()
