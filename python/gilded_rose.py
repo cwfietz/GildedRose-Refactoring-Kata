16850e9adc9f53d3427e3d9482706dced39389bb
Susan Fung <fung_susan@hotmail.com>
2022-04-03 19:13:40 -0600

    Added updateAgedBrie function

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,7 +7,10 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == "Aged Brie":
+                self.updateAgedBrie(item)
+                return
+            if item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         self.decrementQualityByOne(item)
@@ -24,22 +27,22 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                self.decrementQualityByOne(item)
-                    else:
-                        item.quality = item.quality - item.quality
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            self.decrementQualityByOne(item)
                 else:
-                    if item.quality < 50:
-                        self.incrementQualityByOne(item)
+                    item.quality = item.quality - item.quality
 
     def decrementQualityByOne(self, item):
         item.quality = item.quality - 1
 
     def incrementQualityByOne(self, item):
         item.quality = item.quality + 1
+    
+    def updateAgedBrie(self, item):
+        if item.quality < 50:
+            self.incrementQualityByOne(item)
 
 
 class Item:
